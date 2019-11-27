0b05ef427fa4c945b99f40e7a9198227e9ad3450
赵东明 <530882914@qq.com>
2019-11-27 16:30:25 +0800

    提交NIO代码

--- activemq-comsumer-demo/src/main/java/com/zdm/springbootdemo/message/service/Comsumer.java
+++ activemq-comsumer-demo/src/main/java/com/zdm/springbootdemo/message/service/Comsumer.java
@@ -3,6 +3,9 @@ package com.zdm.springbootdemo.message.service;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @class: Comsumer
  * @description:
@@ -15,6 +18,7 @@ public class Comsumer {
     // 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
     @JmsListener(destination = "mytest.queue")
     public void receiveQueue(String text) {
-        System.out.println("Consumer收到的报文为:"+text);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(new Date())+"Consumer收到的报文为:"+text);
     }
 }
--- activemq-producer-demo/src/test/java/com/zdm/springbootdemo/message/service/ProducerTest.java
+++ activemq-producer-demo/src/test/java/com/zdm/springbootdemo/message/service/ProducerTest.java
@@ -9,6 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.jms.Destination;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -24,8 +27,10 @@ public class ProducerTest {
 
         Destination destination = new ActiveMQQueue("mytest.queue");
 
-        for(int i=0; i<100; i++) {
-            producer.sendMessage(destination, "myname is chhliu!!!");
+        for(int i=0; i<1; i++) {
+           //producer.sendMessage(destination, "myname is chhliu!!!");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            producer.delaySend(destination,"哈哈哈" + sdf.format(new Date()),30000L);
         }
 
     }
